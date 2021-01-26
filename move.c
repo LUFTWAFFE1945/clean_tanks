4e2400b69b1a6f80ae6acbdeb6f2e9c9bd43ddfa
qombajn <kacper01deleska@gmail.com>
2021-01-26 03:17:10 +0100

    bla

--- move.c
+++ move.c
@@ -1,16 +1,6 @@
 
 #include "cos.h"
 
-char *info(char *token, maciora *p, Dane *z)
-{
-    char *url = (char *)malloc(sizeof(char) * 1024);
-    strcpy(url, "http://edi.iem.pw.edu.pl:30000/worlds/api/v1/worlds/info");
-    strcat(url, "/");
-    strcat(url, token);
-    uzupelnienie_info(interpret_response(make_request(url), z), p);
-    free(url);
-}
-
 char *explore(char *token, maciora *p, Dane *z)
 {
     char *url = (char *)malloc(sizeof(char) * 1024);
@@ -18,6 +8,16 @@ char *explore(char *token, maciora *p, Dane *z)
     strcat(url, "/");
     strcat(url, token);
     uzupelnienie_explore(interpret_response(make_request(url), z), p);
+    free(url);
+}
+
+char *info(char *token, maciora *p, Dane *z)
+{
+    char *url = (char *)malloc(sizeof(char) * 1024);
+    strcpy(url, "http://edi.iem.pw.edu.pl:30000/worlds/api/v1/worlds/info");
+    strcat(url, "/");
+    strcat(url, token);
+    uzupelnienie_info(interpret_response(make_request(url), z), p);
     free(url);
 }
 
@@ -40,6 +40,7 @@ char *rotate(char *token, char *direction, maciora *p, Dane *z)
     strcat(url, token);
     strcat(url, "/");
     strcat(url, direction);
+    make_request(url);
     uzupelnienie_info(interpret_response(make_request(url), z), p);
     free(url);
 }
